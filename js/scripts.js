fff86724cdca275c3b52aa6bf18a778246b56c40
RamonPage <ramonpage@gmail.com>
2012-06-26 19:46:45 -0300

    Fix slider.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  $("a[rel^='external']").each(function() { $(this).attr("target","_blank"); });
+
   $('nav, .call-to-action').localScroll({
     duration: 1000,
     axis: 'y'

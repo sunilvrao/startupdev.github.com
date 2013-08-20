91bd55c85f03e1750d5edefe8d6d99079b221020
Thiago Gonzalez <contato@thiagogonzalez.com>
2013-08-20 17:22:05 -0300

    How it works done

--- js/mp.js
+++ js/mp.js
@@ -1,3 +1,7 @@
 $(document).on('ready', function() {
 	$('#landing').append('<div class="bg-cover"></div>');
+
+	// last and first child
+	$(':last-child').addClass('last-child');
+	$(':first-child').addClass('first-child');
 })

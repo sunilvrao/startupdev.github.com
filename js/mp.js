b626151bb21f6144f360ac1381377d9ab680133b
Thiago Gonzalez <contato@thiagogonzalez.com>
2013-08-21 11:52:52 -0300

    Projects Gallery and Who we are done

--- js/mp.js
+++ js/mp.js
@@ -1,12 +1,8 @@
 $(document).on('ready', function() {
 	$('#landing').append('<div class="bg-cover"></div>');
+	$('#who-we-are').append('<div class="bg-cover"></div>');
 
 	// last and first child
 	$(':last-child').addClass('last-child');
 	$(':first-child').addClass('first-child');
-})
-
-
-$(document).on('ready', function() {
-  $('#who-we-are').append('<div class="bg-cover"></div>');
-})
+});

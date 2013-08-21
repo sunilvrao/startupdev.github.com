cca6a9c7643a8e97741e989159d253f47c401ece
Thiago Gonzalez <contato@thiagogonzalez.com>
2013-08-21 14:37:58 -0300

    Testimonials with mansory for list

--- js/mp.js
+++ js/mp.js
@@ -5,4 +5,12 @@ $(document).on('ready', function() {
 	// last and first child
 	$(':last-child').addClass('last-child');
 	$(':first-child').addClass('first-child');
+
+	$('#testimonials ul li:nth-child(2n)').addClass('n2');
+
+	// with jQuery
+	$('#testimonials ul').masonry({
+	  columnWidth: 480,
+	  itemSelector: 'li'
+	});
 });

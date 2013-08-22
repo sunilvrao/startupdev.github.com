ab397402bf81b42c476cdcdf775601f48d304c7d
Thiago Gonzalez <contato@thiagogonzalez.com>
2013-08-22 17:42:36 -0300

    Nav fixed in header when scrolling

--- js/mp.js
+++ js/mp.js
@@ -1,15 +1,15 @@
 $(document).on('ready', function() {
 	$('#landing').append('<div class="bg-cover"></div>');
-	$('#who-we-are').append('<div class="bg-cover"></div>');
+	$('.who-we-are').append('<div class="bg-cover"></div>');
 
 	// last and first child
 	$(':last-child').addClass('last-child');
 	$(':first-child').addClass('first-child');
 
-	$('#testimonials ul li:nth-child(2n)').addClass('n2');
+	$('.testimonials ul li:nth-child(2n)').addClass('n2');
 
 	// with jQuery
-	$('#testimonials ul').masonry({
+	$('.testimonials ul').masonry({
 	  columnWidth: 480,
 	  itemSelector: 'li'
 	});
@@ -34,18 +34,16 @@ $(document).on('ready', function() {
 		$('#other-services').slideToggle();
 	});
 
-	$('a[href^="#"]').on('click',function (e) {
+	$('.more a').on('click',function (e) {
 	    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
-
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
+	        'scrollTop': $('.contact').offset().top
+	    }, 900, 'swing');
 	});
+
+
+	$('#nav').smint();
 
 	Shadowbox.init();
 });

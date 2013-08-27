487c5533b3dc2b0e78765d52dfa2cd9e4a33f527
Aluísio Azevedo <aluisio.me@gmail.com>
2013-08-27 12:27:02 -0300

    Changing testmonials presentation from list to slide gallery

--- js/mp.js
+++ js/mp.js
@@ -26,6 +26,17 @@ $(document).on('ready', function() {
 	});
 
 
+	$('#testimonials_gallery ul')
+	.after('<a href="#" id="testimonials_gallery_prev"></a>')
+	.after('<a href="#" id="testimonials_gallery_next"></a>')
+	.cycle({
+		'pause': 1,
+		'timeout': 8000,
+		'prev': '#testimonials_gallery_prev',
+		'next': '#testimonials_gallery_next'
+	});
+
+
 	$('.other-services').click(function(e) {
 		e.preventDefault();
 

286403395de80cb3d979f1375df96bfd18771145
Rodrigo Pinto <rodrigopqn@gmail.com>
2013-11-09 14:58:46 -0200

    Add jquery masked input to service pages to format phone number.

--- js/services.js
+++ js/services.js
@@ -51,4 +51,17 @@ $(document).on('ready', function() {
 	$('#nav').smint();
 
 	Shadowbox.init();
+
+  $("#Field4").focusout(function(){
+    var phone, element;
+    element = $(this);
+    element.unmask();
+    phone = element.val().replace(/\D/g, '');
+    if(phone.length > 10) {
+        element.mask("(99) 99999-999?9");
+    } else {
+        element.mask("(99) 9999-9999?9");
+    }
+  }).trigger('focusout');
+
 });
